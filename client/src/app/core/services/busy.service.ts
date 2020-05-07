a8be483d3ad4247d375afa226a4a29d41af950c6
Wojciech Dudek <wdudek82@gmail.com>
2020-05-07 21:53:08 +0200

    Add home page carousel

--- client/src/app/core/services/busy.service.ts
+++ client/src/app/core/services/busy.service.ts
@@ -12,7 +12,7 @@ export class BusyService {
   busy() {
     this.busyRequestCount++;
     this.spinnerService.show(undefined, {
-      type: 'pacman',
+      type: 'timer',
       bdColor: 'rgba(255, 255, 255, 0.7)',
       color: '#333',
     });
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagerComponent } from './components/pager/pager.component';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
   declarations: [PagerComponent, PagingHeaderComponent],
-  imports: [CommonModule, PaginationModule.forRoot()],
-  exports: [PaginationModule, PagerComponent, PagingHeaderComponent],
+  imports: [CommonModule, PaginationModule.forRoot(), CarouselModule.forRoot()],
+  exports: [
+    PaginationModule,
+    CarouselModule,
+    PagerComponent,
+    PagingHeaderComponent,
+  ],
 })
 export class SharedModule {}
