4c83ba7b2e4e90830d341b1bc3cd0d3a44b7abfd
Wojciech Dudek <wdudek82@gmail.com>
2020-05-10 14:48:15 +0200

    Add order total to basket template

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { Basket, IBasket, IBasketItem } from '../shared/models/basket';
+import {
+  Basket,
+  IBasket,
+  IBasketItem,
+  IBasketTotals,
+} from '../shared/models/basket';
 import { map } from 'rxjs/operators';
 import { IProduct } from '../shared/models/product';
 
@@ -11,8 +16,12 @@ import { IProduct } from '../shared/models/product';
 })
 export class BasketService {
   baseUrl = environment.apiUrl;
+
   private basketSource = new BehaviorSubject<IBasket>(null);
   basket$ = this.basketSource.asObservable();
+
+  private basketTotalSource = new BehaviorSubject<IBasketTotals>(null);
+  basketTotal$ = this.basketTotalSource.asObservable();
 
   constructor(private http: HttpClient) {}
 
@@ -20,29 +29,34 @@ export class BasketService {
     return this.http.get<IBasket>(this.baseUrl + 'basket?id=' + id).pipe(
       map((basket) => {
         this.basketSource.next(basket);
-        console.log(this.getCurrentBasketValue());
+        this.calculateTotals();
       }),
     );
   }
 
   setBasket(basket: IBasket): void {
-    this.http
-      .post<IBasket>(this.baseUrl + 'basket', basket)
-      .subscribe(
-        (updBasket) => {
-          this.basketSource.next(updBasket);
-          console.log(updBasket);
-        },
-        (error) => {
-          console.log(error);
-        },
-      );
+    this.http.post<IBasket>(this.baseUrl + 'basket', basket).subscribe(
+      (updBasket) => {
+        this.basketSource.next(updBasket);
+        this.calculateTotals();
+      },
+      (error) => {
+        console.log(error);
+      },
+    );
   }
 
-  deleteBasket(id: string): void {
-    this.http.delete<void>(this.baseUrl + 'basket?id=' + id).subscribe(() => {
-      this.basketSource.next(null);
-    });
+  deleteBasket(basket: IBasket) {
+    return this.http
+      .delete<void>(this.baseUrl + 'basket?id=' + basket.id)
+      .subscribe(
+        () => {
+          this.basketSource.next(null);
+          this.basketTotalSource.next(null);
+          localStorage.removeItem('basket_id');
+        },
+        (error) => console.log(error),
+      );
   }
 
   getCurrentBasketValue() {
@@ -53,6 +67,26 @@ export class BasketService {
     const itemToAdd = this.mapProductItemToBasketItem(item, quantity);
     const basket = this.getCurrentBasketValue() ?? this.createBasket();
     basket.items = this.addOrUpdateItem(basket.items, itemToAdd, quantity);
+    this.setBasket(basket);
+  }
+
+  incrementItemQuantity(item: IBasketItem): void {
+    const basket = this.getCurrentBasketValue();
+    const foundItemIndex = basket.items.findIndex((i) => i.id === item.id);
+    basket.items[foundItemIndex].quantity++;
+    this.setBasket(basket);
+  }
+
+  decrementItemQuantity(item: IBasketItem): void {
+    const basket = this.getCurrentBasketValue();
+    const foundItemIndex = basket.items.findIndex((i) => i.id === item.id);
+
+    if (basket.items[foundItemIndex].quantity > 1) {
+      basket.items[foundItemIndex].quantity--;
+    } else {
+      this.removeItemFromBasket(item);
+    }
+
     this.setBasket(basket);
   }
 
@@ -92,5 +126,28 @@ export class BasketService {
       productBrand: item.productBrand,
       productType: item.productType,
     };
+  }
+
+  private calculateTotals(): void {
+    const basket = this.getCurrentBasketValue();
+    const shipping = 0;
+    const subtotal = basket.items.reduce(
+      (acc, i) => i.price * i.quantity + acc,
+      0,
+    );
+    const total = subtotal + shipping;
+
+    this.basketTotalSource.next({ shipping, total, subtotal });
+  }
+
+  private removeItemFromBasket(item: IBasketItem): void {
+    const basket = this.basketSource.value;
+    basket.items.filter((i) => i.id !== item.id);
+
+    if (!basket.items.length) {
+      this.deleteBasket(basket);
+    } else {
+      this.setBasket(basket);
+    }
   }
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -4,14 +4,16 @@ import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagerComponent } from './components/pager/pager.component';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
 
 @NgModule({
-  declarations: [PagerComponent, PagingHeaderComponent],
+  declarations: [PagerComponent, PagingHeaderComponent, OrderTotalsComponent],
   imports: [CommonModule, PaginationModule.forRoot(), CarouselModule.forRoot()],
   exports: [
-    PaginationModule,
     CarouselModule,
+    OrderTotalsComponent,
     PagerComponent,
+    PaginationModule,
     PagingHeaderComponent,
   ],
 })
