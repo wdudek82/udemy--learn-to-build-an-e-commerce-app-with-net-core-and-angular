a2c65c444a6ac248255592b7dc8df2de058054d8
Wojciech Dudek <wdudek82@gmail.com>
2020-05-12 00:27:32 +0200

    Add a token generation service

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -36,5 +36,27 @@ namespace API.Controllers
                 DisplayName = user.DisplayName
             };
         }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            var user = new AppUser
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                UserName = registerDto.Email,
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded) return BadRequest(new ApiResponse(400));
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = "This will be a token",
+                DisplayName = user.DisplayName
+            };
+        }
     }
 }
