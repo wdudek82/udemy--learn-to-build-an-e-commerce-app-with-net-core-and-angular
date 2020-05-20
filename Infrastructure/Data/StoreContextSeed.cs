2bd9dc46380330b17df002f4dee05e17f0b01e4b
Wojciech Dudek <wdudek82@gmail.com>
2020-05-20 23:36:16 +0200

    Add Order classes to the StoreContext and create StoreContextSeed

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Entities.OrderAggregate;
 using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Data
@@ -52,6 +53,17 @@ namespace Infrastructure.Data
                     }
 
                     await context.SaveChangesAsync();
+                }
+
+                if (!context.DeliveryMethods.Any())
+                {
+                    var dmData = File.ReadAllText("../Infrastructure/Data/SeedData/delivery.json");
+                    var methods = JsonSerializer.Deserialize<List<DeliveryMethod>>(dmData);
+
+                    foreach (var method in methods)
+                    {
+                        context.DeliveryMethods.Add(method);
+                    }
                 }
             }
             catch (Exception e)
